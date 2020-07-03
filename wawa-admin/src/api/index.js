7a9905ca674ef961af6f6b8ffa772b5c73f8bc04
freeprofession <872371803@qq.com>
2020-07-03 10:22:22 +0800

    vue

--- wawa-admin/src/api/index.js
+++
@@ -1,7 +0,0 @@
-const api = {
-    Login: '/auth/login',
-    Logout: '/auth/logout',
-    // get my info
-    Me: '/auth/me'
-}
-export default api
--- wawa-admin/src/api/qiniu.js
+++
@@ -1,8 +0,0 @@
-import { axios } from '@/utils/request'
-
-export function getQiniuToken() {
-  return axios({
-    url: '/qiniu/token',
-    method: 'get'
-  })
-}
